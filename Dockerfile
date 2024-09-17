82de51754ecb84a2991e3ac14b7286497a9dd214
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-09-17 23:09:24 +0200

    Add path traversal vulnerability

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,12 @@ COPY . .
 
 RUN npm run build
 
+# Create the necessary folder
+RUN mkdir -p /usr/uploads/tenant/
+
+RUN chown -R node:node /usr/uploads/tenant/
+RUN chmod -R 755 /usr/uploads/tenant/
+
 EXPOSE 3000
 
 ENV NODE_ENV production
--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request } from '@nestjs/common';
+import * as fs from 'fs';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('documents')
 export class DocumentsController {
+  constructor(private authService: AuthService) {}
   @Get('preview')
   async create(@Query('url') url: string) {
     try {
@@ -10,6 +13,31 @@ export class DocumentsController {
       return { response: responseBody };
     } catch (error) {
       return { error: 'Failed to fetch the document', details: error.message };
+    }
+  }
+
+  @Get(':path')
+  async fetch(@Request() req, @Param('path') filePath: string) {
+    const user = req.user;
+    const tenantId = await this.authService.getFirstTenant(user.id);
+
+    try {
+      const file = `/usr/uploads/tenant/${tenantId}/files/${filePath}`;
+
+      // Read the file from the file system asynchronously
+      const fileContent = await fs.promises.readFile(file, 'utf-8');
+
+      return {
+        success: true,
+        data: fileContent,
+      };
+    } catch (error) {
+      // Handle errors such as file not found or permission issues
+      return {
+        success: false,
+        message: 'File could not be read',
+        error: error.message,
+      };
     }
   }
 }
