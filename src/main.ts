cceec5b1118fba3e4b92bca74fed00201d7ab6f8
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-10-24 16:00:45 +0200

    Add example.com

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  app.enableCors({credentials: true, origin: ["https://sovulnerable.fly.dev", "https://sovulnerable.fly.dev.evil.com"]})
+  app.enableCors({credentials: true, origin: ["https://sovulnerable.fly.dev", "https://sovulnerable.fly.dev.evil.com", "https://example.com"]})
   await app.listen(3000);
 }
 bootstrap();
