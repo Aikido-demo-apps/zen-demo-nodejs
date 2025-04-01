fec14405184e524352c8f84a155ac56148a70c31
Samuel <samuel@sava.be>
2025-04-01 17:43:07 +0200

    basic routes in place

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,38 @@ app.get('/', (c) => {
   return c.html(fs.readFileSync('static/index.html', 'utf8'));
 })
 
+app.get('/pages/create', (c) => {
+  return c.html(fs.readFileSync('static/create.html', 'utf8'));
+})
+
+app.get('/pages/execute', (c) => {
+  return c.html(fs.readFileSync('static/execute_command.html', 'utf8'));
+})
+
+app.get('/pages/read', (c) => {
+  return c.html(fs.readFileSync('static/read_file.html', 'utf8'));
+})
+
+app.get('/pages/request', (c) => {
+  return c.html(fs.readFileSync('static/request.html', 'utf8'));
+})
+
+app.get('/test_ratelimiting_1', (c) => {
+  return c.text("Request successful (Ratelimiting 1)")
+})
+
+app.get('/test_ratelimiting_2', (c) => {
+  return c.text("Request successful (Ratelimiting 2)")
+})
+
+app.get('/test_bot_blocking', (c) => {
+  return c.text("Hello World! Bot blocking enabled on this route.")
+})
+
+app.get('/test_user_blocking', (c) => {
+  return c.text("Hello User with id: " + c.req.header('user'))
+})
+
 app.use('*', serveStatic({ root: './static/public' }));
 
 serve({
