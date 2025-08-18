df3cee97b9551dac5ef8f44fd67288a7c0258759
PopoviciMarian <marian@aikido.dev>
2025-08-18 17:27:58 +0300

    Update src/index.ts
    Co-authored-by: Timo Kössler <info@timokoessler.de>

--- src/index.ts
+++ src/index.ts
@@ -156,7 +156,7 @@ app.post('/api/request', async (c) => {
     if (error.message.includes("Zen has blocked a server-side request forgery") || error.cause.code === "ENOTFOUND" || error.cause.code === "ERR_INVALID_URL") {
       return c.json({
         success: false,
-        output: error.message,
+        output: `${error.message}${error.cause ? ` (${error.cause})` : ''}`,
       }, 500)
     }
 
